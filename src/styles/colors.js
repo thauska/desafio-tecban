c93a778b154d57c14f0510d46cef97987ebe7e65
Thayana Mamoré <thayanacmamore@gmail.com>
2020-07-25 23:15:16 -0300

    Layout colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,8 +1,14 @@
 export default {
-  background: '#FFFFFF',
-  statusBar: '#FFFFFF',
+  background: '#F2F2F2',
+  statusBar: '#F2F2F2',
   textLight: '#FFFFFF',
   textDark: '#000000',
-  textMono: '#393838DE',
-  buttonBackground: '#76e497',
+  textMono: '#707070',
+  buttonPrimary: '#F2B84B',
+  buttonSecundary: '#F24141',
+  buttonInfo: '#4b299a',
+  buttonText: '#FFFFFF',
+  colorPimary: '#5BD9D9',
+  colorSecundary: '#D9483B',
+  colorTertiary: '#521fc7'
 };
